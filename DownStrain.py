41084ad245adaace2a78e474f88fdfa8f7a40059
DGenady <80817266+DGenady@users.noreply.github.com>
2022-05-22 11:00:02 +0300

    Update DownStrain.py

--- DownStrain.py
+++ DownStrain.py
@@ -14,10 +14,10 @@ args = parser.parse_args()
 
 s3 = boto3.resource('s3',endpoint_url = 'https://s3-west.nrp-nautilus.io')
 
-with open('/download/O1_H1.txt','r') as f:
+with open('gw_torch/download/O1_H1.txt','r') as f:
     H1_list = f.read().split('\n')
     
-with open('/download/O1_L1.txt','r') as f:
+with open('gw_torch/download/O1_L1.txt','r') as f:
     L1_list = f.read().split('\n')
     
 for i in range(args.start_file,args.last_file):
