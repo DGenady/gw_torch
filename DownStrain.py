ae33f1de3c7ab8e89e7815b24d676dc22b840cbb
tomerh <hacohen0tomer@gmail.com>
2023-09-10 13:02:47 +0300

    changed script to download O2 data

--- DownStrain.py
+++ DownStrain.py
@@ -1,8 +1,8 @@
-import os.path
+import os
 import time
 import urllib
-import boto3
 import argparse
+from multiprocessing.pool import ThreadPool
 
 
 parser = argparse.ArgumentParser(description='PyTorch Triplet network')
@@ -12,42 +12,25 @@ parser.add_argument('--last-file', type=int, default=10, metavar='N',
                     help='last file to upload to S3 storage (default: 10)')
 args = parser.parse_args()
 
-s3 = boto3.resource('s3',endpoint_url = 'https://s3-west.nrp-nautilus.io')
+max_files = 10
+project_name = "m4443"
+project_path = f"/global/cfs/cdirs/{project_name}/O2_data/"
 
-with open('gw_torch/download/O1_H1.txt','r') as f:
+with open('./gw_torch/spec_prep/O2_H1.txt','r') as f:
     H1_list = f.read().split('\n')
-    
-with open('gw_torch/download/O1_L1.txt','r') as f:
+
+with open('./gw_torch/spec_prep/O2_L1.txt','r') as f:
     L1_list = f.read().split('\n')
-    
-for i in range(args.start_file,args.last_file):
-    H1_file = H1_list[i]
-    H1_save = 'H1_' + H1_list[i].split('-')[-2] + '.hdf5'
-    urllib.request.urlretrieve(H1_file, f'{H1_save}')
-    
-    L1_file = L1_list[i]
-    L1_save = 'L1_' + L1_list[i].split('-')[-2] + '.hdf5'
-    urllib.request.urlretrieve(L1_file, f'{L1_save}')
-    
-    file_present = False
 
-    while file_present == False:
-        
-        if os.path.isfile(H1_save):
-            s3.meta.client.upload_file(f'{H1_save}','tau-astro', f'gdevit/download/H1/{H1_save}')
-            file_present = True
-            break
-            
-        time.sleep(5)
-        
-    file_present = False
+H1_save_paths = ['H1_' + x.split('-')[-2] + '.hdf5' for x in H1_list]
+L1_save_paths = ['L1_' + x.split('-')[-2] + '.hdf5' for x in L1_list]
 
-    while file_present == False:
-        
-        if os.path.isfile(L1_save):
-            s3.meta.client.upload_file(f'{L1_save}','tau-astro', f'gdevit/download/L1/{L1_save}')
-            file_present = True
-            break
-            
-        time.sleep(5)
-           
+H1_save_paths = [os.path.join(project_path, x) for x in H1_save_paths]
+L1_save_paths = [os.path.join(project_path, x) for x in L1_save_paths]
+
+with ThreadPool(32) as pool:
+    H_combined_list = zip(H1_list, H1_save_paths)[:max_files]
+    L_combined_list = zip(L1_list, L1_save_paths)[:max_files]
+    combined = H_combined_list + L_combined_list
+    done = pool.map(urllib.request.urlretrieve, combined)
+print(done)
