1e8b1ee21f0eaf6cbf40a2cb24253a7e30dcc13c
tomerh <hacohen0tomer@gmail.com>
2023-09-10 14:30:41 +0300

    bug fixes

--- DownStrain.py
+++ DownStrain.py
@@ -1,36 +1,37 @@
 import os
-import time
-import urllib
-import argparse
+import requests
 from multiprocessing.pool import ThreadPool
 
+def download_file(url, save_path):
+    resp = requests.get(url, allow_redirects=True)
+    if resp.status_code != 200:
+        return url, False
+    with open(save_path, "wb") as f:
+        f.write(resp.content)
+    print(f"downloading file {save_path}")
+    return url, True
 
-parser = argparse.ArgumentParser(description='PyTorch Triplet network')
-parser.add_argument('--start-file', type=int, default=0, metavar='N',
-                    help='first file to upload to S3 storage (default: 0)')
-parser.add_argument('--last-file', type=int, default=10, metavar='N',
-                    help='last file to upload to S3 storage (default: 10)')
-args = parser.parse_args()
-
-max_files = 10
+max_files = 500
 project_name = "m4443"
 project_path = f"/global/cfs/cdirs/{project_name}/O2_data/"
 
-with open('./gw_torch/spec_prep/O2_H1.txt','r') as f:
+
+
+with open('./spec_prep/O2_H1.txt','r') as f:
     H1_list = f.read().split('\n')
 
-with open('./gw_torch/spec_prep/O2_L1.txt','r') as f:
+with open('./spec_prep/O2_L1.txt','r') as f:
     L1_list = f.read().split('\n')
 
-H1_save_paths = ['H1_' + x.split('-')[-2] + '.hdf5' for x in H1_list]
-L1_save_paths = ['L1_' + x.split('-')[-2] + '.hdf5' for x in L1_list]
+H1_save_paths = ['H1_' + x.split('-')[-2] + '.hdf5' for x in H1_list if len(x)>0]
+L1_save_paths = ['L1_' + x.split('-')[-2] + '.hdf5' for x in L1_list if len(x)>0]
 
 H1_save_paths = [os.path.join(project_path, x) for x in H1_save_paths]
 L1_save_paths = [os.path.join(project_path, x) for x in L1_save_paths]
 
 with ThreadPool(32) as pool:
-    H_combined_list = zip(H1_list, H1_save_paths)[:max_files]
-    L_combined_list = zip(L1_list, L1_save_paths)[:max_files]
+    H_combined_list = list(zip(H1_list, H1_save_paths))[:max_files]
+    L_combined_list = list(zip(L1_list, L1_save_paths))[:max_files]
     combined = H_combined_list + L_combined_list
-    done = pool.map(urllib.request.urlretrieve, combined)
+    done = pool.starmap(download_file, combined)
 print(done)
